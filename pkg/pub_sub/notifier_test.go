5e101457ec952619b2049b8752859098e97ea25d
Jakob Lorz <jakob@lorz.org>
2021-12-24 15:45:58 +0100

    Add notify test

--- pkg/pub_sub/notifier_test.go
+++ pkg/pub_sub/notifier_test.go
@@ -18,3 +18,18 @@ func Test_notifier_Subscribe(t *testing.T) {
 	n.Subscribe(tf)
 	assert.Equal(t, reflect.ValueOf(tf).Pointer(), reflect.ValueOf(n.s[0]).Pointer())
 }
+
+func Test_notifier_Notify(t *testing.T) {
+	sig := make(chan struct{}, 1)
+	tf := func() {
+		close(sig)
+	}
+
+	n := &notifier{
+		RWMutex: *new(sync.RWMutex),
+		s:       make([]func(), 0),
+	}
+	n.Subscribe(tf)
+	n.Notify()
+	<-sig
+}
