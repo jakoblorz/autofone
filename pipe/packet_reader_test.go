ada763a058464f8276d764a7a8cefc1c795692ca
Jakob Lorz <jakob@lorz.org>
2021-12-24 11:03:57 +0100

    Fix packet_reader test

--- pipe/packet_reader_test.go
+++ pipe/packet_reader_test.go
@@ -7,20 +7,49 @@ import (
 	"testing"
 	"time"
 
-	"github.com/bxcodec/faker/v3"
 	"github.com/jakoblorz/metrikxd/constants"
 	"github.com/jakoblorz/metrikxd/packets"
 	"github.com/jakoblorz/metrikxd/pkg/udptest"
 	"github.com/stretchr/testify/assert"
 )
 
-func createPopulatedPacket(pack interface{}) interface{} {
-	faker.FakeData(pack)
-	return pack
-}
+// func createPopulatedPacket(pack interface{}, packetId uint8) interface{} {
+// 	faker.FakeData(pack)
+// 	return pack
+// }
 
 var (
-	motionData = *createPopulatedPacket(&packets.PacketMotionData{
+	// motionData = *createPopulatedPacket(&packets.PacketMotionData{
+	// 	Header: packets.PacketHeader{
+	// 		PacketFormat:            2020,
+	// 		GameMajorVersion:        1,
+	// 		GameMinorVersion:        18,
+	// 		PacketVersion:           1,
+	// 		PacketID:                0,
+	// 		SessionUID:              11855624319420004949,
+	// 		SessionTime:             1.5710948,
+	// 		FrameIdentifier:         43,
+	// 		PlayerCarIndex:          19,
+	// 		SecondaryPlayerCarIndex: 255,
+	// 	},
+	// 	CarMotionData:          [22]packets.CarMotionData{}, // empty
+	// 	SuspensionPosition:     [4]float32{-0.6432814, 0.049691506, -0.122375205, -0.11044062},
+	// 	SuspensionVelocity:     [4]float32{-5.7109776, -2.5368745, -0.33160102, 1.6033937},
+	// 	SuspensionAcceleration: [4]float32{-742.09515, -298.6635, -38.07375, 275.05835},
+	// 	WheelSpeed:             [4]float32{120.0, 120.0, 120.0, 120.0},
+	// 	WheelSlip:              [4]float32{10.5, 5.3, 9.5, 9.5},
+	// 	LocalVelocityX:         -0.00016372708,
+	// 	LocalVelocityY:         -0.0011803857,
+	// 	LocalVelocityZ:         0.0015708461,
+	// 	AngularVelocityX:       0.0113855535,
+	// 	AngularVelocityY:       -0.00053515914,
+	// 	AngularVelocityZ:       0.0073023424,
+	// 	AngularAccelerationX:   1.5726409,
+	// 	AngularAccelerationY:   0.047205403,
+	// 	AngularAccelerationZ:   1.0070984,
+	// 	FrontWheelsAngle:       0,
+	// }, constants.PacketMotion).(*packets.PacketMotionData)
+	motionData = packets.PacketMotionData{
 		Header: packets.PacketHeader{
 			PacketFormat:            2020,
 			GameMajorVersion:        1,
@@ -49,7 +78,7 @@ var (
 		AngularAccelerationY:   0.047205403,
 		AngularAccelerationZ:   1.0070984,
 		FrontWheelsAngle:       0,
-	}).(*packets.PacketMotionData)
+	}
 )
 
 func TestPacketReader_read(t *testing.T) {
