df1abd4b746e9bb80b737d245098d0cd2a0588be
Jakob Lorz <jakob@lorz.org>
2022-01-01 15:30:22 +0100

    Add commented test from local

---
+++ packets/marshal_packet.go
@@ -0,0 +1,10 @@
+package packets
+
+// import (
+// 	"encoding/binary"
+// 	"io"
+// )
+
+// func marshalToWriter(w io.Writer, pack interface{}) error {
+// 	return binary.Write(w, binary.LittleEndian, pack)
+// }
---
+++ packets/unmarshal.go
@@ -0,0 +1,10 @@
+package packets
+
+// import (
+// 	"encoding/binary"
+// 	"io"
+// )
+
+// func unmarshalToPacket(r io.Reader, pack interface{}) error {
+// 	return binary.Read(r, binary.LittleEndian, pack)
+// }
